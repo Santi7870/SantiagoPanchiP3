7091fc893eb2fedd7f2ee735b483ebadb007ff49
Santi7870 <santiago.panchi@udla.edu.ec>
2025-01-21 07:49:55 -0500

    commit4

--- SantiagoPanchiP3/ViewModels/SearchViewModel.cs
+++ SantiagoPanchiP3/ViewModels/SearchViewModel.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Windows.Input;
+using System.Collections.ObjectModel;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
 using SantiagoPanchiP3.Models;
 using SantiagoPanchiP3.Data;
+using System.IO;
+using System;
 
 namespace SantiagoPanchiP3.ViewModels
 {
@@ -14,14 +17,28 @@ namespace SantiagoPanchiP3.ViewModels
 
         public ObservableCollection<Movie> Movies { get; set; } = new ObservableCollection<Movie>();
 
+        public string SearchText { get; set; }
+
+        public ICommand SearchCommand { get; }
+        public ICommand ClearCommand { get; }
+
         public SearchViewModel()
         {
             string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "spanchi_movies.db");
             _movieDatabase = new MovieDatabase(dbPath);
+
+            SearchCommand = new Command(async () => await SearchMovieAsync(SearchText));
+            ClearCommand = new Command(ClearSearch);
         }
 
         public async Task SearchMovieAsync(string query)
         {
+            if (string.IsNullOrWhiteSpace(query))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "Por favor ingresa un nombre de película", "OK");
+                return;
+            }
+
             string url = $"https://freetestapi.com/api/v1/movies?search={query}&limit=1";
             var response = await _httpClient.GetStringAsync(url);
             var movies = JsonSerializer.Deserialize<List<Movie>>(response);
@@ -31,9 +48,17 @@ namespace SantiagoPanchiP3.ViewModels
                 var movie = movies.First();
                 await _movieDatabase.SaveMovieAsync(movie);
             }
+            else
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "No se encontró la película", "OK");
+            }
+        }
+
+        private void ClearSearch()
+        {
+            SearchText = string.Empty;
         }
     }
-
-
 }
+
 
