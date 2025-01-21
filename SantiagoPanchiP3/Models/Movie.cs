aae23ba893f1cda6c20fa67ee2b52b3c397de947
Santi7870 <santiago.panchi@udla.edu.ec>
2025-01-21 07:36:47 -0500

    commit1

---
+++ SantiagoPanchiP3/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+
+namespace SantiagoPanchiP3.Models
+{
+    public class Movie
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Genero { get; set; }
+        public string ActorPrincipal { get; set; }
+        public string Awards { get; set; }
+        public string Website { get; set; }
+        public string Spanchi { get; set; }
+    }
+}
---
+++ SantiagoPanchiP3/ViewModels/SearchViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using SantiagoPanchiP3.Models;
+using SantiagoPanchiP3.Data;
+
+namespace SantiagoPanchiP3.ViewModels
+{
+    public class SearchViewModel
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+        public ObservableCollection<Movie> Movies { get; set; } = new ObservableCollection<Movie>();
+
+        public async Task SearchMovieAsync(string query)
+        {
+            string url = $"https://freetestapi.com/api/v1/movies?search={query}&limit=1";
+            var response = await _httpClient.GetStringAsync(url);
+            var movies = JsonSerializer.Deserialize<List<Movie>>(response);
+
+            if (movies != null && movies.Any())
+            {
+                var movie = movies.First();
+                await MovieDatabase.SaveMovieAsync(movie);
+            }
+        }
+    }
+
